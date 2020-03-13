425f025e000c3bf54dee396d1f546a9188197e5a
Alex <alex.lapeiretta@gmail.com>
2020-03-13 11:12:03 -0400

    test: Add MovieTest for TDD of Movie model

---
+++ app/src/main/java/com/krinotech/popularmoviesstageone/model/Movie.java
@@ -0,0 +1,41 @@
+package com.krinotech.popularmoviesstageone.model;
+
+public class Movie {
+    private String title;
+    private String imageUrl;
+    private String releaseDate;
+    private double voteAverage;
+    private String plotSynopsis;
+
+    // Default constructor for serialization
+    public Movie() {}
+
+    public Movie(String title, String imageUrl,
+                 String releaseDate, double voteAverage, String plotSynopsis) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.releaseDate = releaseDate;
+        this.voteAverage = voteAverage;
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+}
---
+++ app/src/test/java/com/krinotech/popularmoviesstageone/MovieTest.java
@@ -0,0 +1,59 @@
+package com.krinotech.popularmoviesstageone;
+
+import com.krinotech.popularmoviesstageone.model.Movie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MovieTest {
+    private String titleParam = "Jaws";
+    private String imageUrlParam = "movieDatabase";
+    private String releaseDateParam = "1965";
+    private double voteAverage = 4.9;
+    private String plotSynopsis = "A deadly shark, a deadly game.";
+
+    private Movie testSubject;
+
+    @Before
+    public void setUp() {
+        testSubject = new Movie(
+                titleParam,
+                imageUrlParam,
+                releaseDateParam,
+                voteAverage,
+                plotSynopsis);
+    }
+
+    @Test
+    public void movie_should_have_empty_constructor(){
+        new Movie();
+    }
+
+    @Test
+    public void movie_getTitle_should_return_title() {
+        assertEquals(testSubject.getTitle(), titleParam);
+    }
+
+    @Test
+    public void movie_getImageUrl_should_return_imageUrl() {
+        assertEquals(testSubject.getImageUrl(), imageUrlParam);
+    }
+
+    @Test
+    public void movie_getReleaseDate_should_return_releaseDate() {
+        assertEquals(testSubject.getReleaseDate(), releaseDateParam);
+    }
+
+    @Test
+    public void movie_getVoteAverage_should_return_voteAverage() {
+        assertEquals(testSubject.getVoteAverage(), voteAverage, 0.01);
+    }
+
+    @Test
+    public void movie_getPlotSynopsis_should_return_plotSynopsis() {
+        assertEquals(testSubject.getPlotSynopsis(), plotSynopsis);
+    }
+}
