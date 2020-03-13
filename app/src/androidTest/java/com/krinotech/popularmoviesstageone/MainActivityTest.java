5fce8691c6faf21fc659ef21cfe7cf07f87258bc
Alex <alex.lapeiretta@gmail.com>
2020-03-13 14:58:17 -0400

    feat: Add title 'Pop Movies' to conform to specification requirement

---
+++ app/src/androidTest/java/com/krinotech/popularmoviesstageone/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.krinotech.popularmoviesstageone;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    public ActivityTestRule<MainActivity> testSubject =
+            new ActivityTestRule<>(MainActivity.class, false, false);
+
+    @Test
+    public void launch_should_see_title() {
+        // Context of the app under test.
+        testSubject.launchActivity(null);
+
+        String title = "Pop Movies";
+
+        onView(withText(title)).check(matches(isDisplayed()));
+    }
+}
